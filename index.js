39d6366981b38add0fa2de6132b13e7311b66674
Franc Auxach Cortés <francauxach@gmail.com>
2021-10-23 16:21:39 +0200

    Added reversedDiagonal functionallities

--- index.js
+++ index.js
@@ -22,66 +22,76 @@ console.table(matrix)
 
 // Determine if X items consecutively equals horizontally
 const horizontallyMap = matrix.map((col) => col.join('').includes([...Array(consecutiveOcurrences)].map(() => 'A').join('')) || col.join('').includes([...Array(consecutiveOcurrences)].map(() => 'B').join('')))
-// console.log(horizontallyMap)
+console.log(horizontallyMap)
 
 // Determine if X items consecutively equals vertically (transposing the matrix)
 const transposedMatrix = matrix[0].map((item, index) => matrix.map(item => item[index]))
 const verticallyMap = transposedMatrix.map((col) => col.join('').includes([...Array(consecutiveOcurrences)].map(() => 'A').join('')) || col.join('').includes([...Array(consecutiveOcurrences)].map(() => 'B').join('')))
-// console.log(verticallyMap)
+console.log(verticallyMap)
 
 // Determine if X items consecutively equals diagonally (from left to right -> bottom)
 // Discard some diagonals by consecutiveOcurrences number
-const maxColsToSearchDiagonal = Math.round(cols / 2)
-const maxRowsToSearchDiagonal = Math.round(rows / 2)
-const diagonalsToBottom = []
-const diagonalsToRight = []
+function findByDiagonal(matrix, reverse = false) {
+    const cols = matrix[0].length
+    const rows = matrix.length
+    const maxColsToSearchDiagonal = Math.round(cols / 2)
+    const maxRowsToSearchDiagonal = Math.round(rows / 2)
+    const diagonalsToBottom = []
+    const diagonalsToRight = []
 
-for (let index = 0; index < maxRowsToSearchDiagonal; index++) {
-    const arrayValues = []
+    for (let index = 0; index < maxRowsToSearchDiagonal; index++) {
+        const arrayValues = []
 
-    for (let indexRows = index; indexRows < rows; indexRows++) {
-        const diagonalValues = []
+        for (let indexRows = index; indexRows < rows; indexRows++) {
+            const diagonalValues = []
 
-        for (let indexCols = (indexRows - index); indexCols < cols; indexCols++) {
-            diagonalValues.push(matrix[indexRows][indexCols])
+            for (let indexCols = (indexRows - index); indexCols < cols; indexCols++) {
+                diagonalValues.push(matrix[indexRows][indexCols])
+            }
+
+            if (diagonalValues.length) {
+                arrayValues.push(diagonalValues.join('')[0])
+            }
         }
 
-        if (diagonalValues.length) {
-            arrayValues.push(diagonalValues.join('')[0])
+        if (arrayValues.length >= consecutiveOcurrences) {
+            diagonalsToBottom.push(arrayValues)
         }
     }
 
-    if (arrayValues.length >= consecutiveOcurrences) {
-        diagonalsToBottom.push(arrayValues)
+    for (let index = 1; index < maxColsToSearchDiagonal; index++) {
+        const arrayValues = []
+
+        for (let indexRows = 0; indexRows < rows; indexRows++) {
+            const diagonalValues = []
+        
+            for (let indexCols = (indexRows + index); indexCols < cols; indexCols++) {
+                diagonalValues.push(matrix[indexRows][indexCols])
+            }
+
+            if (diagonalValues.length) {
+                arrayValues.push(diagonalValues.join('')[0])
+            }
+        }
+
+        if (arrayValues.length >= consecutiveOcurrences) {
+            diagonalsToRight.push(arrayValues)
+        }
     }
+
+    return diagonalsToBottom.concat(diagonalsToRight)
 }
-
-for (let index = 1; index < maxColsToSearchDiagonal; index++) {
-    const arrayValues = []
-
-    for (let indexRows = 0; indexRows < rows; indexRows++) {
-        const diagonalValues = []
-    
-        for (let indexCols = (indexRows + index); indexCols < cols; indexCols++) {
-            diagonalValues.push(matrix[indexRows][indexCols])
-        }
-
-        if (diagonalValues.length) {
-            arrayValues.push(diagonalValues.join('')[0])
-        }
-    }
-
-    if (arrayValues.length >= consecutiveOcurrences) {
-        diagonalsToRight.push(arrayValues)
-    }
-}
-
 // Concat both diagonals
-const diagonalsMatrix = diagonalsToBottom.concat(diagonalsToRight)
+const diagonalsMatrix = findByDiagonal(matrix)
 // console.table(diagonalsMatrix)
 
 // Result
 const diagonallyMap = diagonalsMatrix.map((col) => col.join('').includes([...Array(consecutiveOcurrences)].map(() => 'A').join('')) || col.join('').includes([...Array(consecutiveOcurrences)].map(() => 'B').join('')))
-// console.log(diagonallyMap)
+console.log(diagonallyMap)
 
 // Determine if X items consecutively equals diagonally (from left to right -> top)
+const reversedMatrix = matrix.reverse()
+const reversedDiagonalsMatrix = findByDiagonal(reversedMatrix, true)
+const reversedDiagonallyMap = reversedDiagonalsMatrix.map((col) => col.join('').includes([...Array(consecutiveOcurrences)].map(() => 'A').join('')) || col.join('').includes([...Array(consecutiveOcurrences)].map(() => 'B').join('')))
+console.log(reversedDiagonallyMap)
+
